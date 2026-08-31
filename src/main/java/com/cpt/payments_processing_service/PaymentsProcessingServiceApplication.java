247e98d4250fe15a49cf59d2f6ea7f0b2dc5a556
KUN4L2001 <kunalgaikwad0257@gmail.com>
2026-08-31 11:14:15 +0530

    first commit

---
+++ src/main/java/com/cpt/payments_processing_service/PaymentsProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cpt.payments_processing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsProcessingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpt/payments_processing_service/PaymentsProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpt.payments_processing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
